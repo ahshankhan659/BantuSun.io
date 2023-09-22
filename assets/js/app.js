e8d991725279507b467a644765d872404e5c0452
ahshankhan659 <akahshankhan659@gmail.com>
2023-09-22 04:10:08 -0700

    initialze

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+
+$( document ).ready(function() {
+
+
+
+    // FOR  MODAL
+    $('.imgOnhover').mouseover(function () {
+        $('.pop-up-modal').addClass('show');
+    });
+
+    $('.pop-up-button').click(function () {
+        $('.pop-up-modal').removeClass('show');
+    });
+   
+
+    // FOR  MODAL 1
+    $('.imgOnhover-1').mouseover(function () {
+        $('.pop-up-modal-1').addClass('show');
+    });
+
+    $('.pop-up-button-1').click(function () {
+        $('.pop-up-modal-1').removeClass('show');
+    });
+   
+    // FOR  MODAL 2
+    $('.imgOnhover-2').mouseover(function () {
+        $('.pop-up-modal-2').addClass('show');
+    });
+
+    $('.pop-up-button-2').click(function () {
+        $('.pop-up-modal-2').removeClass('show');
+    });
+   
+    // FOR  MODAL 3
+    $('.imgOnhover-3').mouseover(function () {
+        $('.pop-up-modal-3').addClass('show');
+    });
+
+    $('.pop-up-button-3').click(function () {
+        $('.pop-up-modal-3').removeClass('show');
+    });
+   
+
+    // FOR  MODAL 4
+    $('.imgOnhover-5').mouseover(function () {
+        $('.pop-up-modal-4').addClass('show');
+    });
+
+    $('.pop-up-button-4').click(function () {
+        $('.pop-up-modal-4').removeClass('show');
+    });
+   
+});
